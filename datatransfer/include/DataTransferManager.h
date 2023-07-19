221e28c9c820eb102fc5122c340feb99fdbb2880
phil wilshire <pwilshire@flexgen.com>
2023-07-19 08:41:06 +0000

    build works

--- datatransfer/include/DataTransferManager.h
+++ datatransfer/include/DataTransferManager.h
@@ -1,8 +1,10 @@
+#pragma once
 
 #include <functional>
 #include <vector>
 #include <chrono>
 #include <string>
+#include <thread>
 
 enum class TransferMode {
     IMMEDIATE,
@@ -10,9 +12,7 @@ enum class TransferMode {
     PERIODIC_WITHOUT_INPUT
 };
 
-class DataTransferManager {
-public:
-    union Data {
+struct Data {
         int intData;
         float floatData;
         bool boolData;
@@ -22,18 +22,122 @@ public:
         ~Data() {}
     };
 
+class DataTransferManager {
+public:
+    // union Data {
+    //     int intData;
+    //     float floatData;
+    //     bool boolData;
+    //     std::string stringData;
+
+    //     Data() {}
+    //     ~Data() {}
+    // };
+
     using DataCallback = std::function<void(Data, std::chrono::system_clock::time_point)>;
 
-    DataTransferManager(TransferMode mode, int periodMs = 1000);
-    ~DataTransferManager();
 
-    void setInputCallback(DataCallback callback);
-    void addData(int data);
-    void addData(float data);
-    void addData(bool data);
-    void addData(const std::string& data);
-    void start();
-    void stop();
+
+DataTransferManager(TransferMode mode, int periodMs)
+    : mode_(mode), periodMs_(periodMs), running_(false) {
+    lastDataInputs_.reserve(1024);
+    timestamps_.reserve(1024);
+};
+
+~DataTransferManager() {
+    stop();
+};
+
+// void setInputCallback(DataCallback callback) {
+//     inputCallback_ = callback;
+// }
+
+void addData(int data) {
+    Data newData;
+    newData.intData = data;
+    addData(newData);
+}
+
+void addData(float data) {
+    Data newData;
+    newData.floatData = data;
+    addData(newData);
+}
+
+void addData(bool data) {
+    Data newData;
+    newData.boolData = data;
+    addData(newData);
+}
+
+void addData(const std::string& data) {
+    Data newData;
+    newData.stringData = data;
+    addData(newData);
+}
+
+void addData(Data data) {
+    if (lastDataInputs_.size() >= 1024) {
+        lastDataInputs_.erase(lastDataInputs_.begin());
+        timestamps_.erase(timestamps_.begin());
+    }
+
+    lastDataInputs_.push_back(data);
+    timestamps_.push_back(std::chrono::system_clock::now());
+}
+
+void start() {
+    if (running_)
+        return;
+
+    running_ = true;
+    transferThread_ = std::thread(&DataTransferManager::transferData, this);
+}
+
+void stop() {
+    if (!running_)
+        return;
+
+    running_ = false;
+    if (transferThread_.joinable()) {
+        transferThread_.join();
+    }
+}
+private:
+void transferData() {
+    while (running_) {
+        switch (mode_) {
+            case TransferMode::IMMEDIATE: {
+                if (inputCallback_ && !lastDataInputs_.empty()) {
+                    inputCallback_(lastDataInputs_.back(), timestamps_.back());
+                }
+                break;
+            }
+            case TransferMode::PERIODIC_WITH_INPUT: {
+                if (inputCallback_ && !lastDataInputs_.empty()) {
+                    inputCallback_(lastDataInputs_.back(), timestamps_.back());
+                }
+                break;
+            }
+            case TransferMode::PERIODIC_WITHOUT_INPUT: {
+                if (inputCallback_) {
+                    if (!lastDataInputs_.empty()) {
+                        inputCallback_(lastDataInputs_.back(), timestamps_.back());
+                    } else {
+                        Data emptyData;
+                        inputCallback_(emptyData, std::chrono::system_clock::now());
+                    }
+                }
+                break;
+            }
+        }
+
+        if (inputCallback_ && mode_ != TransferMode::IMMEDIATE) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(periodMs_));
+        }
+    }
+}
+
 
 private:
     TransferMode mode_;
@@ -44,5 +148,4 @@ private:
     std::thread transferThread_;
     bool running_;
 
-    void transferData();
 };
--- datatransfer/src/main.cpp
+++ datatransfer/src/main.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include "DataTransferManager.h"
 
-void inputDataCallback(int data, std::chrono::system_clock::time_point timestamp) {
-    std::cout << "Received data: " << data << " Timestamp: "
-              << std::chrono::system_clock::to_time_t(timestamp) << std::endl;
-}
+// void inputDataCallback(int data, std::chrono::system_clock::time_point timestamp) {
+//     std::cout << "Received data: " << data << " Timestamp: "
+//               << std::chrono::system_clock::to_time_t(timestamp) << std::endl;
+// }
 
 int main() {
     DataTransferManager dataTransfer(TransferMode::PERIODIC_WITH_INPUT, 1000);
-    dataTransfer.setInputCallback(inputDataCallback);
-    dataTransfer.start();
+    // dataTransfer.setInputCallback(inputDataCallback);
+    // dataTransfer.start();
 
-    // Simulate data production
-    for (int i = 1; i <= 10; ++i) {
-        dataTransfer.addData(i);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    // // Simulate data production
+    // for (int i = 1; i <= 10; ++i) {
+    //     dataTransfer.addData(i);
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // }
 
-    dataTransfer.stop();
+    // dataTransfer.stop();
     return 0;
 }
