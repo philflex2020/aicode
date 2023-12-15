3abab9cd5f5b08a39c33c679b9f678fd7ca0527a
Phil Wilshire <pwilshire@flexgen.com>
2023-12-14 21:59:06 -0500

    staggered pyess working even better and better

--- pifile/pyess.py
+++ pifile/pyess.py
@@ -33,13 +33,20 @@ def ess_master(arg1):
 # Define another thread function for example
 def bms_master(arg1):
     myStore = get_store(arg1)
+    print(f" bms myStore {myStore}")
     myStore["uri"] = arg1
     count = 0
-    myStore["count"] = 0
+    if "count" not in myStore:
+        myStore["count"] = 0
+    else:
+        count = myStore["count"]
+
     while True:  # 'True' should be capitalized
+        mycount = myStore.get("mycount", 0)
+        count = myStore["count"]
         count += 1
         myStore["count"] = count
-        print("             bms_running :"+ arg1 +" count "+ str(count))
+        print("             bms_running :"+ arg1 + " count "+ str(count) + " mycount "+ str(mycount))
         time.sleep(5)
     # Implement bms_master logic here
 
@@ -71,9 +78,17 @@ def update_data_store(uri, body):
     last_key = keys[-1]
     if last_key in current_level and isinstance(current_level[last_key], dict):
         # If the last key exists and its value is a dictionary, merge the dictionaries
-        current_level[last_key].update(body)
+        print(f" merge last key {last_key}")
+        print(body)
+        for mkey in body:
+            print(mkey)
+            print(last_key)
+            #current_level[last_key].update(body)
+            current_level[last_key][mkey] = body[mkey]
     else:
         # Otherwise, just set the value to body
+        print(f" set last key {last_key}")
+        print(body)
         current_level[last_key] = body
     #current_level[keys[-1]] = body
 
@@ -104,22 +119,53 @@ def echo_server(host, port):
                             body = json.loads(jbody)
                         else :
                             body = jbody   
-                        #print("Keys in body:")
-                        #for key in body:
-                        #    print(key)
+                        print("Keys in body:")
+                        for key in body:
+                            print(key)
 
                         # Process the data
-                        update_data_store(uri, body)
                         if method == "run":
+                            update_data_store(uri, body)
+
                             if 'type' in body:
                                 #print(f"Received type: {body['type']}")
                                 start_thread(body['type'],uri)
+                            conn.sendall(data)
 
-                        print(f"Data store updated: {data_store}")
+                        if method == "show":
+                            #print(f"Data store updated: {data_store}")
+                            data = json.dumps(data_store, indent=4)
+                            conn.sendall(data.encode())
+
+                        if method == "get":
+                            #print(f"Data store updated: {data_store}")
+                            myStore = get_store(uri)
+                            print(f"uri: {uri}")
+                            print(f"Mystore: {myStore}")
+                            data = json.dumps(myStore)
+                            conn.sendall(data.encode())
+                        if method == "set":
+                            update_data_store(uri, body)
+                            myStore = get_store(uri)
+                            data = json.dumps(myStore)
+                            conn.sendall(data.encode())
+
                     except json.JSONDecodeError:
                         print("Invalid JSON received")
 
-                    conn.sendall(data)
+                    #conn.sendall(data)
+
+def my_server(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen()
+        print(f"Server listening on {host}:{port}")
+
+        while True:
+            conn, addr = s.accept()
+            threading.Thread(target=handle_client, args=(conn, addr)).start()
+
 
 if __name__ == "__main__":
     HOST = '127.0.0.1'
