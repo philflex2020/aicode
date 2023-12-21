a25e296114b837792c04e27fbad5db10bee2ff4e
Phil Wilshire <pwilshire@flexgen.com>
2023-12-20 22:31:46 -0500

    enhancements

--- datamesh/src/DataMeshServer.py
+++ datamesh/src/DataMeshServer.py
@@ -6,66 +6,10 @@ import signal
 import sys
 from DataMeshUtils import *
 
-# data_store = {}
 
-# def is_valid_json(test_string):
-#     try:
-#         json_object = json.loads(test_string)
-#         return True
-#     except json.JSONDecodeError:
-#         return False
-
-# def get_store(uri):
-#     keys = uri.strip("/").split("/")
-#     current_level = data_store
-#     if uri == '/':
-#         return current_level
-
-#     #for key in keys[:-1]:
-#     for key in keys:
-#         if key not in current_level:
-#             current_level[key] = {}
-#         current_level = current_level[key]
-#     return current_level
-
-# def get_last(uri):
-#     keys = uri.strip("/").split("/")
-#     return keys[-1]
-
-# def update_data_store(uri, body):
-#     keys = uri.strip("/").split("/")
-#     current_level = data_store
-
-#     for i, key in enumerate(keys):
-#         if i == len(keys) - 1:  # If it's the last key
-#             if is_valid_json(body):
-#                 #print(" body is json")
-#                 new_data = json.loads(body)
-#                 if isinstance(new_data, dict) and isinstance(current_level.get(key, {}), dict):
-#                     print("Merge dictionaries")
-#                     current_level[key] = merge_dicts(current_level.get(key, {}), new_data)
-#                 else:
-#                     # Replace the existing value
-#                     current_level[key] = new_data
-#             else:
-#                 print("If body is not a JSON string, just assign the value")
-#                 current_level[key] = body
-#         else:
-#             # Navigate or create new nested dictionary
-#             current_level = current_level.setdefault(key, {})
-
-# def merge_dicts(original, new_data):
-#     for key, value in new_data.items():
-#         if key in original and isinstance(original[key], dict) and isinstance(value, dict):
-#             original[key] = merge_dicts(original[key], value)
-#         else:
-#             original[key] = value
-#     return original
-
-def handle_set(dm, data):
+def handle_set(dm, uri, data):
     print(f"Handling 'set' with data: {data}")
     body = data["body"]
-    uri = data["uri"]
     jbody = json.loads(body)
     update_data_store(uri, body)
 
@@ -74,20 +18,18 @@ def handle_set(dm, data):
     dm.sendall("set ok".encode())
 
 
-def handle_show(dm, data):
-    uri = data["uri"]
+def handle_show(dm, uri, data):
     myStore = get_store(uri)
     data = json.dumps(myStore, indent=4)
     dm.sendall(data.encode())
 
-def handle_run(dm, data):
+def handle_run(dm, uri, data):
     print(f"Handling 'run' with data: {data}")
     json_data = json.dumps({"command": "run"})
     dm.sendall(json_data.encode())
 
-def handle_get(dm,data):
+def handle_get(dm,uri,data):
     body = data["body"]
-    uri = data["uri"]
     myStore = get_store(uri)
     sdata = json.dumps(myStore)
     dm.sendall(sdata.encode())
@@ -119,15 +61,16 @@ class DataMeshServer:
             if message.get("message") == "hello":
                 print("Received hello from DataMeshController")
                 # Respond to hello message or perform some initialization
-            elif "method" in message:
+            elif "method" in message and "uri" in message:
+                uri = message["uri"]
                 if message["method"] == "set":
-                    handle_set(client_socket, message)
+                    handle_set(client_socket, uri, message)
                 elif message["method"] == "run":
-                    handle_run(client_socket,message)
+                    handle_run(client_socket,uri, message)
                 elif message["method"] == "get":
-                    handle_get(client_socket,message)
+                    handle_get(client_socket,uri, message)
                 elif message["method"] == "show":
-                    handle_show(client_socket,message)
+                    handle_show(client_socket,uri, message)
             else:
                 print("Unknown message type received")
         client_socket.close()
